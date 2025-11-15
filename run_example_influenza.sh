0f654f2581779f10144f06f0cd363e46c255ce4d
michall <mlazniewski@gmail.com>
2025-11-15 21:51:13 +0100

    chore(tests): clean up example run scripts for sample data

--- run_example_influenza.sh
+++ run_example_influenza.sh
@@ -8,8 +8,9 @@
 bash nf_pipeline_viral_phylo.sh -i ./data/example_data/influenza/ \
                                 -m ~/git/pzh-phylogenetic-pipeline/data/example_data/influenza/influenza_metadata.tsv \
 				-g influenza \
-			       	-p test_influenza \
+			  -p test_influenza \
 				-d . \
 				--threshold_Ns 0.05 \
+				--results_dir results \
 				-x slurm 
 
